f8f7a2bba40794db4e95e9b76e9f4081dcf1793a
Andreya <avalabek@yahoo.com>
2017-11-28 13:22:28 -0700

    display one question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,137 +1,101 @@
-    // jquery
+ // jquery
     $(document).ready(function() {
         //declare functions at top of scripts and declare variables at top of functions
         
-        //variables
-        //build an empty prototype for questions
-        var questions = { question: undefined, choices: undefined, answer: undefined };
-        
-        //make a constructor for questions prob change this to some other function that is same for all
-        function Question(triviaQuestion, possibleChoices, correctAnswer) {
-            this.question = triviaQuestion;
-            this.choices = possibleChoices;
-            this.answer = correctAnswer;
-            console.log(triviaQuestion);
-            console.log(possibleChoices);
-            console.log(correctAnswer);
-        }
-        var oneQuestion = new Question("Which of these is not a mountain range in Spain?", ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"], 3);
-        var twoQuestion = new Question("What city has a beach called 'Kite Surf Beach'?", ["Dubai", "Sydney", "Dover", "Santa Monica"], 2);
-        var threeQuestion = new Question("The Chesapeake Bay does not run through which of these states?", ["Delaware", "Maryland", "Virginia", "New Jersey"], 3);
-        var fourQuestion = new Question("Which four countries surround Lake Chad?", ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"], 0);
-        var fiveQuestion = new Question("What is the largest desert in the world?", ["Gobi", "Arabian", "Sahara", "Sonoran"], 0);
-        var sixQuestion = new Question("Which city in France is the home of Airbus?", ["Albi", "Perpignon", "Toulouse", "Carcassone"], 2);
+//variables
+var questions = [{
+        question: "Which of these is not a mountain range in Spain?",
+        choices: ["Sierra Nevada", " Picos de Europa", " Pyrenees", " Alps"],
+        correctAnswer: "Alps"
+    }, {
+        question: "What city has a beach called 'Kite Surf Beach'?",
+        choices: ["Dubai", "Sydney", "Dover", "Santa Monica"],
+        correctAnswer: "Dubai"
+    }, {
+        question: "The Chesapeake Bay does not run through which of these states?",
+        choices: ["Delaware", "Maryland", "Virginia", "New Jersey"],
+        correctAnswer: "New Jersey"
+    }, {
+        question: "Which four countries surround Lake Chad?",
+        choices: ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"],
+        correctAnswer: "Chad, Cameroon, Niger, and Nigeria"
+    },
+    {
+        question: "What is the largest desert in the world?",
+        choices: ["Gobi", "Arabian", "Sahara", "Sonoran"],
+        correctAnswer: "Sahara"
+    },
+    {
+        question: "Which city in France is the home of Airbus?",
+        choices: ["Albi", "Perpignon", "Toulouse", "Carcassone"],
+        correctAnswer: "Toulouse"
+    }
+]
+console.log(questions[0]["question"]);
+console.log(questions[0]["choices"]);
+console.log(questions[0].correctAnswer);
 
 
-        Question.prototype = {
-            // all of these functions will be available to all of the questions these are dummy functions testing
-            // win: function(){alert("You win! You got more right than wrong.")}
-            // lose: function(){alert("You lose! You got more wrong than right.")}
-                }
+$(".question").append(questions[0]["question"]);
+$("#choice1").append(questions[0]["choices"][0]);
+$("#choice2").append(questions[0]["choices"][1]);
+$("#choice3").append(questions[0]["choices"][2]);
+$("#choice4").append(questions[0]["choices"][3]);
+//closing jquery from top
+});
 
-        var wins = 0;
-        var losses = 0;
-        var unanswered = 0;
-        var userGuess;
-        var correctAnswer;
-
-
-        // FUNCTIONS
-        var startGame;
-        var resetGame;
+// var wins = 0;
+//         var losses = 0;
+//         var unanswered = 0;
+//         var userGuess;
+//         var correctAnswer;
 
 
-        startGame = function() {
-        //User clicks to start game
+//         // FUNCTIONS
+//         var startGame;
+//         var resetGame;
 
-         //if you want randomized questions:            
-         // var random = questions[Math.floor(Math.random() * questions.length)];
-        
-        //question with answer choices appears
-        $("#quiz").append(question);
+
+//         startGame = function() {
+//         //User clicks to start game
+// //question with answer choices appears
+//         $("#quiz").append(question);
             
-        //countdown timer starts 
+//         //countdown timer starts 
         
-        //users choice is saved for correct/incorrect tally and unanswered
+//         //users choice is saved for correct/incorrect tally and unanswered
         
-        //next question is displayed, old question hidden
+//         //next question is displayed, old question hidden
    
             
-        };
-        nextQuestion = function(){
-            // correct answer given
-            // new question appears without user input
-            //win/lose/unanswered incremeneted
-        }
+//         };
+//         nextQuestion = function(){
+//             // correct answer given
+//             // new question appears without user input
+//             //win/lose/unanswered incremeneted
+//         }
 
-        resetGame = function() {
-            //Resets wins and losses and unanswered counter and restarts timer
-            wins = 0;
-            losses = 0;
-            unanswered = 0;
-
-            
-
-            startGame();
-        }
-
-        // EVENT LISTENERS
-
-
-        $("#next").click(function() {
-            nextQuestion();
-        })
-
+//         resetGame = function() {
+//             //Resets wins and losses and unanswered counter and restarts timer
+//             wins = 0;
+//             losses = 0;
+//             unanswered = 0;
 
             
-        
-            // increment counters based on win/lose/unanswered
-            // if (userGuess===correct answer) how do i tell it the answer for each one?{
-            //     win++ 
-            // }
 
-            // else(userGuess !==correctAnswer){
-            //     lose++
-            // }
-            // else (the input ===no imput){
-            //     unanswered++
-            // }
+//             startGame();
+//         }
 
-            
-            // update counter on HTML
-            $("#score").text(counter);
-
-            // check win or loss how do i do this? 
-            if (userGuess === correctAnswer) {
-                $("#results").text("You win!").css("color", "blue");
-                ++wins;
-                $("#wins").text(wins);
-//also show picture
-
-                $("#results").on('click', function() {
-                    $("#score").text("");
-
-                    resetGame();
-                    
-                })
-
-            } else if (userGuess===correctAnswer) {
-
-                $("#winOrLose").text("You lose! Click here to play again.").css("color", "red");
-                ++losses;
-                $("#losses").text(losses);
+//         // EVENT LISTENERS
 
 
-                $("#winOrLose").on('click', function() {
-                    $("#score").text("");
-                    resetGame();
-                    //location.reload();
+//         $("#next").click(function() {
+//             nextQuestion();
+//         })
 
-                })
-            }
-        });
-        //Now call the function to start the game
-        startGame();
+       
+//         //Now call the function to start the game
+//         startGame();
     
     
     
