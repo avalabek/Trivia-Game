dcb8da3dfeef66202206be20e0b10e242febf327
Andreya <avalabek@yahoo.com>
2017-11-27 20:33:12 -0700

    add trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,35 @@
+//declare functions at top of scripts and declare variables at top of functions    
+//quiz questions in an array
 var questions = [{
-    question: "Which of these is not a mountain range in Spain?",
-    choices: ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"],
-    correctAnswer: 3
-}, {
-    question: "What city has a beach called 'Kite Surf Beach'?",
-    choices: ["Dubai", "Sydney", "Dover", "Santa Monica"],
-    correctAnswer: 2
-}, {
-    question: "The Chesapeake Bay does not run through which of these states?",
-    choices: ["Delaware", "Maryland", "Virginia", "New Jersey"],
-    correctAnswer: 3
-}, {
-    question: "What is the longest river?",
-    choices: ["Nile", "Amazon", "Mississippi", "Yangtze"],
-    correctAnswer: 0
-}, {
-    question: "What is the busiest tube station in the London?",
-    choices: ["Waterloo", "Baker Street", "Kings Cross", "Victoria"],
-    correctAnswer: 0
-}];
+        question: "Which of these is not a mountain range in Spain?",
+        choices: ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"],
+        correctAnswer: 3
+    }, {
+        question: "What city has a beach called 'Kite Surf Beach'?",
+        choices: ["Dubai", "Sydney", "Dover", "Santa Monica"],
+        correctAnswer: 2
+    }, {
+        question: "The Chesapeake Bay does not run through which of these states?",
+        choices: ["Delaware", "Maryland", "Virginia", "New Jersey"],
+        correctAnswer: 3
+    }, {
+        question: "Which four countries surround Lake Chad?",
+        choices: ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"],
+        correctAnswer: 0
+    },
+    {
+        question: "What is the largest desert in the world?",
+        choices: ["Gobi", "Arabian", "Sahara", "Sonoran"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which city in France is the home of Airbus?",
+        choices: ["Albi", "Perpignon", "Toulouse", "Carcassone"],
+        correctAnswer: 2
+    }
+]
 
-var currentQuestion = 0;
-var correctAnswers = 0;
-var quizOver = false;
-$(document).ready(function () {
 
-    
 //User clicks to start game
 //questions cycle through in random?
 //question with answer appears
@@ -41,4 +45,5 @@ $(document).ready(function () {
 //reloading game clears out users choices and scores and randomizes
 //questions
 //watch demo and make sure this happens
+
 
