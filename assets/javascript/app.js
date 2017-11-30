111f1f29d6d8b2bb5a3be0c4df77cfe2c23a5047
Andreya <avalabek@yahoo.com>
2017-11-30 10:05:32 -0700

    radio buttons user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,174 +1,117 @@
-// jquery
-    $(document).ready(function() {
-        //declare functions at top of scripts and declare variables at top of functions
-        
-//variables
 var questions = [{
         question: "Which of these is not a mountain range in Spain?",
         choices: ["Sierra Nevada", " Picos de Europa", " Pyrenees", " Alps"],
-        correctAnswer: "Alps"
+        correctAnswer: 3
     }, {
         question: "What city has a beach called 'Kite Surf Beach'?",
         choices: ["Dubai", "Sydney", "Dover", "Santa Monica"],
-        correctAnswer: "Dubai"
+        correctAnswer: 0
     }, {
         question: "The Chesapeake Bay does not run through which of these states?",
         choices: ["Delaware", "Maryland", "Virginia", "New Jersey"],
-        correctAnswer: "New Jersey"
+        correctAnswer: 3
     }, {
         question: "Which four countries surround Lake Chad?",
         choices: ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"],
-        correctAnswer: "Chad, Cameroon, Niger, and Nigeria"
+        correctAnswer: 0
     },
     {
         question: "What is the largest desert in the world?",
         choices: ["Gobi", "Arabian", "Sahara", "Sonoran"],
-        correctAnswer: "Sahara"
+        correctAnswer: 2
     },
     {
         question: "Which city in France is the home of Airbus?",
         choices: ["Albi", "Perpignon", "Toulouse", "Carcassone"],
-        correctAnswer: "Toulouse"
+        correctAnswer: 2
     }
 ]
-console.log(questions[0]["question"]);
-console.log(questions[0]["choices"]);
-console.log(questions[0].correctAnswer);
 
 
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
 
-        
-        var random;
-        var wins = 0;
-        var losses = 0;
-        var unanswered = 0;
-        var userGuess;
-        var correctAnswer;
+$(document).ready(function () {
 
+    // Display the first question
+    setInterval(displayCurrentQuestion(),3000);
+    $(this).find(".quizMessage").hide();
 
-        // FUNCTIONS
-        var startGame;
-        var resetGame;
-        var nextQuestion;
-        
-        
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
 
-        startGame = function() {
-           // var random = questions[Math.floor(Math.random() * questions.length)];
+            value = $("input[type='radio']:checked").val();
 
-            $(".question").text(questions[0]["question"]);
-            $("#choice1").text(questions[0]["choices"][0]);
-            $("#choice2").text(questions[0]["choices"][1]);
-            $("#choice3").text(questions[0]["choices"][2]);
-            $("#choice4").text(questions[0]["choices"][3]);
-        //randomize questions        
-        
+            if (value == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } else {
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
 
-        //question with answer choices appears, should be hidden before or empty
-        // $("#quiz").append(question);
-            
-        //countdown timer starts 
-        setTimeout(nextQuestion, 3000);
-        
-        //users choice is saved for correct/incorrect tally and unanswered
-        
-        //next question is displayed, old question hidden
-   
-            
-        };
-        nextQuestion = function(){
-            // correct answer given
-            $(".question").text(questions[1]["question"]);
-            $("#choice1").text(questions[1]["choices"][0]);
-            $("#choice2").text(questions[1]["choices"][1]);
-            $("#choice3").text(questions[1]["choices"][2]);
-            $("#choice4").text(questions[1]["choices"][3]);
-            // new question appears without user input
-            //win/lose/unanswered incremeneted
-        setTimeout(nextQuestion3,3000)
-        
-        };
+                if (value == questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                }
 
-        nextQuestion3 = function(){
-            $(".question").text(questions[2]["question"]);
-            $("#choice1").text(questions[2]["choices"][0]);
-            $("#choice2").text(questions[2]["choices"][1]);
-            $("#choice3").text(questions[2]["choices"][2]);
-            $("#choice4").text(questions[2]["choices"][3]);
-setTimeout(nextQuestion4,3000)
-        };
-        nextQuestion4 = function(){
-            $(".question").text(questions[3]["question"]);
-            $("#choice1").text(questions[3]["choices"][0]);
-            $("#choice2").text(questions[3]["choices"][1]);
-            $("#choice3").text(questions[3]["choices"][2]);
-            $("#choice4").text(questions[3]["choices"][3]);
-setTimeout(nextQuestion5,3000)
-        };
-nextQuestion5 = function(){
-            $(".question").text(questions[4]["question"]);
-            $("#choice1").text(questions[4]["choices"][0]);
-            $("#choice2").text(questions[4]["choices"][1]);
-            $("#choice3").text(questions[4]["choices"][2]);
-            $("#choice4").text(questions[4]["choices"][3]);
-setTimeout(nextQuestion6,3000)
-        };
-        nextQuestion6 = function(){
-            $(".question").text(questions[5]["question"]);
-            $("#choice1").text(questions[5]["choices"][0]);
-            $("#choice2").text(questions[5]["choices"][1]);
-            $("#choice3").text(questions[5]["choices"][2]);
-            $("#choice4").text(questions[5]["choices"][3]);
-setTimeout(correct,3000)
-        };
-
-correct = function(){
-    $("#results").append("You lose because you chose nothing. The game didn't let you!")
-      }
-
-        resetGame = function() {
-            //Resets wins and losses and unanswered counter and restarts timer
-            wins = 0;
-            losses = 0;
-            unanswered = 0;
-
-            
-
-            startGame();
+                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    //                    $(document).find(".nextButton").toggle();
+                    //                    $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(document).find(".nextButton").text("Play Again?");
+                    quizOver = true;
+                }
+            }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(document).find(".nextButton").text("Next Question");
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
         }
+    });
 
-        // EVENT LISTENERS
-        $("#play").click(function(){
-            startGame();
-        })
-
-        $("#next").click(function() {
-            nextQuestion();
-        })
-
-        // closing jquery from top
 });
- 
 
-       
-        //Now call the function to start the game but when this is active, game breaks right now
-//         startGame();
-    
-    
-    
-
-
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
 
     
-     //when correct answer guessed, answer appears and correct! text
-    //when answer is wrong, answer appears and wrong! text
-    //if no choice is made before time up, answer appears and time up! text
-    // tally is incremented after each turn
-    //when time is up, and answer appears, next question comes up automatically (timer?) no user input
-    //after all questions score is displayed with correct/incorrect/unanswered
-    //play again button appears when clicked, game restarts
-    //restarting game clears out users choices and scores and randomizes
-    //questions?
-    //watch demo and make sure this happens
 
-    
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+
+    var choice;
+    for (i = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+    }
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+    $(document).find(".result").hide();
+}
