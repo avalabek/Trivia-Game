64237cc09ffec66f0cf5ec7913d715818183ed3e
Andreya <avalabek@yahoo.com>
2017-12-01 11:46:58 -0700

    restructured code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,205 +1,143 @@
 // jquery
-    $(document).ready(function() {
-        //declare functions at top of scripts and declare variables at top of functions
-        
-//variables
-var questions = [{
-        question: "Which of these is not a mountain range in Spain?",
-        choices: ["Sierra Nevada", " Picos de Europa", " Pyrenees", " Alps"],
-        correctAnswer: "Alps"
-    }, {
-        question: "What city has a beach called 'Kite Surf Beach'?",
-        choices: ["Santa Monica", "Sydney", "Dubai", "Dover" ],
-        correctAnswer: "Dubai"
-    }, {
-        question: "The Chesapeake Bay does not run through which of these states?",
-        choices: ["New Jersey", "Delaware", "Maryland", "Virginia"],
-        correctAnswer: "New Jersey"
-    }, {
-        question: "Which four countries surround Lake Chad?",
-        choices: ["Chad, Zimbabwe, Somali, and Sierra Leone","Chad, Cameroon, Niger, and Nigeria", "Chad, Togo, Congo, and Ghana","Chad, Mali, Sudan, and Gabon",],
-        correctAnswer: "Chad, Cameroon, Niger, and Nigeria"
-    },
-    {
-        question: "What is the largest desert in the world?",
-        choices: ["Sonoran","Arabian","Sahara","Gobi" ],
-        correctAnswer: "Sahara"
-    },
-    {
-        question: "Which city in France is the home of Airbus?",
-        choices: ["Carcassone", "Perpignon", "Toulouse","Albi" ],
-        correctAnswer: "Toulouse"
+$(document).ready(function() {
+    //declare functions at top of scripts and declare variables at top of functions
+
+    //variables
+    var userGuess;
+    var gameOver = false;
+    //build an empty prototype for questions
+    var questions = { question: undefined, choices: undefined, answer: undefined };
+
+    //make a constructor for questions prob change this to some other function that is same for all
+    function Question(triviaQuestion, possibleChoices, correctAnswer) {
+        this.question = triviaQuestion;
+        this.choices = possibleChoices;
+        this.answer = correctAnswer;
+        console.log(triviaQuestion);
+        console.log(possibleChoices);
+        console.log(correctAnswer);//these don't work
+    
     }
-]
-        
-        var wins = 0;
-        var losses = 0;
-        var unanswered = 0;
-        var userGuess;
-        var correctAnswer;
 
-        // FUNCTIONS
-        var startGame;
-        var resetGame;
-        //var nextQuestion;
+    Question.prototype = {
+        // all of these functions will be available to all of the questions these are dummy functions testing 
+        // win: function(){alert("You win! You got more right than wrong.");}
+        //lose: function(){alert("You lose! You got more wrong than right.");}
+    
+    };
 
-        startGame = function() {
-           
-            $(".question").text(questions[0]["question"]);
-            $("#choice1").text(questions[0]["choices"][0]);
-            $("#choice2").text(questions[0]["choices"][1]);
-            $("#choice3").text(questions[0]["choices"][2]);
-            $("#choice4").text(questions[0]["choices"][3]);              
-                       
-        //countdown timer starts,next question is displayed, old question hidden
-        setTimeout(answer, 5000);
+    function Game() {
+        this.correct;
+        this.wrong;
+        this.currentQuestion;
+        this.questions = [];
+        this.questionTimer;
+        this.answerTimer;
+    }
+
+    Game.prototype = {
+        startGame: function() {
+            // initialize game values
+            this.correct = 0;
+            this.wrong = 0;
+            this.currentQuestion = 0; // first question in array
+
+            // add questions to array
+            this.questions.push(new Question("Which of these is not a mountain range in Spain?", ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"], 3));
+            this.questions.push(new Question("What city has a beach called 'Kite Surf Beach'?", ["Dubai", "Sydney", "Dover", "Santa Monica"], 2));
+            this.questions.push(new Question("The Chesapeake Bay does not run through which of these states?", ["Delaware", "Maryland", "Virginia", "New Jersey"], 3));
+            this.questions.push(new Question("Which four countries surround Lake Chad?", ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"], 0));
+            this.questions.push(new Question("What is the largest desert in the world?", ["Gobi", "Arabian", "Sahara", "Sonoran"], 0));
+            this.questions.push(new Question("Which city in France is the home of Airbus?", ["Albi", "Perpignon", "Toulouse", "Carcassone"], 2));
+
+            // display first question
+            this.showQuestion();
+
+          
             
-   //TODO users choice is saved for correct/incorrect tally and unanswered
+            //next question is displayed, old question hidden
+            showQuestion()
+
+        },
+console.log(game);
+        showQuestion: function() {
+            
+        //question with answer choices appears
+            $(".question").text(this.questions.question);
+            $("#answer1").text(this.questions.possibleChoices[0]);
+            $("#answer2").text(this.questions.possibleChoices[1]);
+            $("#answer3").text(this.questions.possibleChoices[2]);
+            $("#answer4").text(this.questions.possibleChoices[3]);
+
                    
-        };
-        answer = function(){
-            $("#results").text("The Alps is the correct answer.");
-           $("#results").append("<img src='assets/images/alps.jpg' style=>");
-           setTimeout(nextQuestion, 5000);
-         };
+            //starts timer for user to answer one question
+            questionTimer = setTimeout(showAnswer, 3000);
+        },
 
-        nextQuestion = function(){
-            // correct answer given
-            $("#results").hide();
-            $(".question").text(questions[1]["question"]);
-            $("#choice1").text(questions[1]["choices"][0]);
-            $("#choice2").text(questions[1]["choices"][1]);
-            $("#choice3").text(questions[1]["choices"][2]);
-            $("#choice4").text(questions[1]["choices"][3]);
-            // new question appears without user input
-            //win/lose/unanswered incremeneted
-        setTimeout(nextAnswer,5000)
-        
-         };
-         nextAnswer = function(){
-            $("#results").show();
-            $("#results").text("Dubai is the correct answer.");
-           $("#results").append("<img src='assets/images/albada3.jpg'>");
-           setTimeout(nextQuestion3, 5000);
-         };
+        showAnswer: function() {
+            // display answer
+            $("#results").text("The correct answer is " + game.questions[game.currentQuestion].correctAnswer + ".")
 
-        nextQuestion3 = function(){
-            $("#results").hide();
-            $(".question").text(questions[2]["question"]);
-            $("#choice1").text(questions[2]["choices"][0]);
-            $("#choice2").text(questions[2]["choices"][1]);
-            $("#choice3").text(questions[2]["choices"][2]);
-            $("#choice4").text(questions[2]["choices"][3]);
-        setTimeout(nextAnswer3,5000)
-        };
-        nextAnswer3 = function(){
-            $("#results").show();
-            $("#results").text("New Jersey is the correct answer.");
-           $("#results").append("<img src='assets/images/chesapeake.jpg'>");
-           setTimeout(nextQuestion4, 5000);
-         };
-        nextQuestion4 = function(){
-            $("#results").hide();
-            $(".question").text(questions[3]["question"]);
-            $("#choice1").text(questions[3]["choices"][0]);
-            $("#choice2").text(questions[3]["choices"][1]);
-            $("#choice3").text(questions[3]["choices"][2]);
-            $("#choice4").text(questions[3]["choices"][3]);
-        setTimeout(nextAnswer4,5000)
-        };
-        nextAnswer4 = function(){
-            $("#results").show();
-            $("#results").text("Chad, Cameroon, Niger, and Nigeria is the correct answer.");
-           $("#results").append("<img src='assets/images/alquadra.jpg'>");
-           setTimeout(nextQuestion5, 5000);
-         };
-        nextQuestion5 = function(){
-            $("#results").hide();
-            $(".question").text(questions[4]["question"]);
-            $("#choice1").text(questions[4]["choices"][0]);
-            $("#choice2").text(questions[4]["choices"][1]);
-            $("#choice3").text(questions[4]["choices"][2]);
-            $("#choice4").text(questions[4]["choices"][3]);
-        setTimeout(nextAnswer5, 5000)
-        };
-        nextAnswer5 = function(){
-            $("#results").show();
-            $("#results").text("The Sahara is the correct answer.");
-           $("#results").append("<img src='assets/images/bigred.JPG'>");
-           setTimeout(nextQuestion6, 5000);
-         };
+            // increment currentQuestion
+            currentQuestion++;
 
-        nextQuestion6 = function(){
-            $("#results").hide();
-            $(".question").text(questions[5]["question"]);
-            $("#choice1").text(questions[5]["choices"][0]);
-            $("#choice2").text(questions[5]["choices"][1]);
-            $("#choice3").text(questions[5]["choices"][2]);
-            $("#choice4").text(questions[5]["choices"][3]);
-        setTimeout(nextAnswer6,5000)
-        };
-        nextAnswer6 = function(){
-            $("#results").show();
-            $("#results").text("Toulouse is the correct answer.");
-           $("#results").append("<img src='assets/images/toulouse2.jpg'>");
-           setTimeout(gameOver, 5000);
-         };
+            // show next question
+            answerTimer = setTimeout(showQuestion, 3000);
+            //users choice is saved for correct/incorrect tally and unanswered
+            if (userGuess===this.questions.correctAnswer){
+                correct++
+             }else {
+                wrong++
+            }
+            $("#tally").text("You got " + correct + "questions correct and " + wrong + "questions wrong.")
+        }
+    }
 
-        gameOver = function(){
-        $("#results").hide();
-        $("#results").show();
-        $("#results").text("Game over! Click to play again.");
-        $(".question").hide(); 
-        $("#choice1").hide();
-        $("#choice2").hide();
-        $("#choice3").hide();
-        $("#choice4").hide();
+
     
-        }
+    //     startGame();
+    // }
 
-        resetGame = function() {
-            //Resets wins and losses and unanswered counter and restarts timer
-            wins = 0;
-            losses = 0;
-            unanswered = 0;
+    // EVENT LISTENERS
 
-            
+    //User clicks to start game
+    $("#play").click(function() {
+        //Now call the function to start the game
+        Game.prototype.startGame();
+    });
 
-            startGame();
-        }
+    // user clicks one of the answers
+    $(".answer").click(function() {
+        Game.prototype.showAnswer();
 
-        // EVENT LISTENERS
-        $("#play").click(function(){
-            startGame();
-        })
+    });
 
-        $("#next").click(function() {
-            nextQuestion();
-        })
+// TESTING TO SEE IF TEXT GOES IN BUTTONS CORRECTLY:WORKS
+// $(".question").text("questions here");
+// $("#answer1").text("and how about this?");
+// $("#answer2").text("and how about this?");
+// $("#answer3").text("and how about this?");
+// $("#answer4").text("and how about this?");
 
-        // closing jquery from top
+
+  var game = new Game();  
 });
- 
-
-       
-        //Now call the function to start the game but when this is active, game breaks right now
-//         startGame();
-    
-    
-    
 
 
 
-    
-     //when correct answer guessed, answer appears and correct! text
-    //when answer is wrong, answer appears and wrong! text
-    //if no choice is made before time up, answer appears and time up! text
-    // tally is incremented after each turn
-    //when time is up, and answer appears, next question comes up automatically (timer?) no user input
-    //after all questions score is displayed with correct/incorrect/unanswered
-    //play again button appears when clicked, game restarts
-    //restarting game clears out users choices and scores and randomizes
-    //questions?
-    //watch demo and make sure this happens
 
-    
+
+
+
+
+
+//when correct answer guessed, answer appears and correct! text
+//when answer is wrong, answer appears and wrong! text
+//if no choice is made before time up, answer appears and time up! text
+// tally is incremented after each turn
+//when time is up, and answer appears, next question comes up automatically (timer?) no user input
+//after all questions score is displayed with correct/incorrect/unanswered
+//play again button appears when clicked, game restarts
+//restarting game clears out users choices and scores and randomizes
+//questions?
+//watch demo and make sure this happens
