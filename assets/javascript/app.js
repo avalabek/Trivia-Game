e0ef74c7df29a50fc53165f0b5d87f276c367ef0
Andreya <avalabek@yahoo.com>
2017-12-01 23:00:34 -0700

    timing adjustments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,19 @@ $(document).ready(function() {
         
     var userGuess;
     var gameOver = false;
-    var currentQuestion = [];
+    var currentQuestion;
     var wrong = 0;
     var correct = 0;
 
     //build an empty prototype for questions
-    var questions = { question: undefined, choices: undefined, answer: undefined };
+    var questions = { question: undefined, choices: undefined, answer: undefined, photo: undefined};
 
     //make a constructor for questions prob change this to some other function that is same for all
-    function Question(triviaQuestion, possibleChoices, correctAnswer) {
+    function Question(triviaQuestion, possibleChoices, correctAnswer, pictureURL) {
         this.question = triviaQuestion;
         this.choices = possibleChoices;
         this.answer = correctAnswer;
+        this.photo = pictureURL;
         // console.log(triviaQuestion);
         // console.log(possibleChoices);
         // console.log(correctAnswer); //these don't work
@@ -50,17 +51,24 @@ $(document).ready(function() {
 
             
             // // //     // add questions to array
-             this.questions.push(new Question("Which of these is not a mountain range in Spain?", ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"], "Alps"));
-             this.questions.push(new Question("What city has a beach called 'Kite Surf Beach'?", ["Dubai", "Sydney", "Dover", "Santa Monica"], 2));
-             this.questions.push(new Question("The Chesapeake Bay does not run through which of these states?", ["Delaware", "Maryland", "Virginia", "New Jersey"], 3));
-             this.questions.push(new Question("Which four countries surround Lake Chad?", ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"], 0));
-             this.questions.push(new Question("What is the largest desert in the world?", ["Gobi", "Arabian", "Sahara", "Sonoran"], 0));
-             this.questions.push(new Question("Which city in France is the home of Airbus?", ["Albi", "Perpignon", "Toulouse", "Carcassone"], 2));
+             this.questions.push(new Question("Which of these is not a mountain range in Spain?", ["Sierra Nevada", "Picos de Europa", "Pyrenees", "Alps"], "Alps","<img src='assets/images/alps.jpg'>"));
+             this.questions.push(new Question("What city has a beach called 'Kite Surf Beach'?", ["Dubai", "Sydney", "Dover", "Santa Monica"], "Dubai", "<img src='assets/images/albadaa.jpg'>"));
+             this.questions.push(new Question("The Chesapeake Bay does not run through which of these states?", ["Delaware", "Maryland", "Virginia", "New Jersey"], "New Jersey","<img src='assets/images/chesapeake.jpg'>" ));
+             this.questions.push(new Question("Which four countries surround Lake Chad?", ["Chad, Cameroon, Niger, and Nigeria", "Chad, Mali, Sudan, and Gabon", "Chad, Zimbabwe, Somali, and Sierra Leone", "Chad, Togo, Congo, and Ghana"], "Chad,Cameroon,Niger, and Nigeria", "<img src='assets/images/alquadra.jpg'>"));
+             this.questions.push(new Question("What is the largest desert in the world?", ["Gobi", "Arabian", "Sahara", "Sonoran"], "Sahara","<img src='assets/images/bigred.jpg'>"));
+             this.questions.push(new Question("Which city in France is the home of Airbus?", ["Albi", "Perpignon", "Toulouse", "Carcassone"], "Toulouse", "<img src='assets/images/toulouse.jpg'>"));
 
             // display first question
             
             setInterval(this.showQuestion(), 3000);
-         
+         var numChoices = this.questions[this.currentQuestion].choices.length;
+    var choice;
+    for (vari = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        this.startGame();
+        }
+
+
         },
 
 
@@ -81,14 +89,15 @@ $(document).ready(function() {
 
         showAnswer: function() {
             // console.log(game);
+            clearTimer = clearInterval(questionTimer)
             $("#results").text("The correct answer is " + game.questions[game.currentQuestion].answer + ".");
-            $("#results").append("<img src='assets/images/alps.jpg'>");
+            $("#results").append(this.questions[this.currentQuestion].photo);
 
             // increment currentQuestion
             currentQuestion++;
 
             // show next question
-            answerTimer = setTimeout(this.showQuestion, 2000);
+            answerTimer = setInterval(this.showQuestion, 2000);
             //users choice is saved for correct/incorrect tally and unanswered
             if (("data-choice") == this.questions[this.currentQuestion].answer) {
                 correct++;
